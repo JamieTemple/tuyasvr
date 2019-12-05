7235fb6710da7607ae81abb87664cd84d5567703
Jamie Temple <jamie.temple@parcs.org.uk>
2019-12-05 21:06:03 +0000

    latest tweaks

---
+++ RegisterService.js
@@ -0,0 +1,23 @@
+﻿
+// https://github.com/coreybutler/node-windows
+
+var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'Tuya Server',
+  description: 'The nodejs.org example web server.',
+  script: 'C:\\OpenHAB2\\tuyasvr\\server.js',
+  nodeOptions: [
+    '--harmony',
+    '--max_old_space_size=4096'
+  ]
+});
+
+// Listen for the "install" event, which indicates the
+// process is available as a service.
+svc.on('install',function(){
+  svc.start();
+});
+
+svc.install();
---
+++ UnunstallService.js
@@ -0,0 +1,16 @@
+﻿var Service = require('node-windows').Service;
+
+// Create a new service object
+var svc = new Service({
+  name:'Tuya Server',
+  script: require('path').join(__dirname,'start.js')
+});
+
+// Listen for the "uninstall" event so we know when it's done.
+svc.on('uninstall',function(){
+  console.log('Uninstall complete.');
+  console.log('The service exists: ',svc.exists);
+});
+
+// Uninstall the service.
+svc.uninstall();
